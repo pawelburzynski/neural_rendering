2de97c2bf1023f03d94932c263cdfeaa35bbe3c0
Pawel Burzynski <pawel.burzynski98@gmail.com>
2020-04-22 02:29:35 +0100

    prepare code for new renderer

--- main/Simple_Render/SimpleRenderer.h
+++ main/Simple_Render/SimpleRenderer.h
@@ -1,5 +1,5 @@
-#ifndef LightFieldRenderer_H
-#define LightFieldRenderer_H
+#ifndef SimpleRenderer_H
+#define SimpleRenderer_H
 
 #include <QBrush>
 #include <QFont>
@@ -7,67 +7,24 @@
 #include <QWidget>
 #include <QVector3D>
 #include <QMatrix4x4>
+#include "Renderer23.h"
 
 #define __CL_ENABLE_EXCEPTIONS
 #include <CL/cl.hpp>
 
-class SimpleRenderer
+
+class SimpleRenderer : public Renderer23
 {
 public:
     SimpleRenderer();
 
 public:
     void paint(QPainter *painter, QPaintEvent *event, int elapsed, const QSize &destSize);
-    void readData(const char *data_dir);
     void generateEvaluationOutput(const char *data_dir, const char* output_dir);
 
-    float focus = 500.0f;
-    float apertureSize = 8.0f;
-    QVector3D K_pos; // Camera position
-    QVector3D K_pos_0; // Initial cmera position
-	int viewWidth, viewHeight; // Resolution of the rendered image in pixels
-
-	float camera_fov = 90.f;
-
 private:
-    void updateViewSize(int newWidth, int newHeight);
-
-    // OpenCL stuff 
-    void initOpenCL();
-    void prepareCamPosArr();
-    void calculateAllTransforms();
-    cl::Platform platform;
-    cl::Device device;
-    cl::Context context;
-    cl::Program program;
-    cl::CommandQueue queue;
-    cl::Kernel kernel;
-    cl::Image2D vcamImage;
-
-    //  Renderer Data
-    cl::Image3D renderData;
-    int imgWidth = -1;
-    int imgHeight = -1;
-    int training_dataPoints = 0;
-    int eval_dataPoints = 0;
-    int debug = 0;
-    const int number_closest_points = 10;
-
-    // Input data
-    void readImages(QString dir,  std::vector<QVector4D>* w_cam, QStringList* data_files);
-    QStringList training_data;
-    QStringList eval_data;
-    
-    // Tranformation Matrices
-    std::vector<QMatrix4x4> Vi;
-    std::vector<QVector4D> w_cam_training; // World coordindates of each camera
-	std::vector<QVector4D> w_cam_eval;
-    std::vector<float> camPosArr;
-    std::vector<int> closestCamArr;
-    cl::Buffer curPos;
-    cl::Buffer camPos;      // World coordindates of each camera
-    cl::Buffer closestCam; 
+   
 };
 
 
-#endif // LightFieldRenderer_H
+#endif // SimpleRenderer_H
--- main/Simple_Render/main.cpp
+++ main/Simple_Render/main.cpp
@@ -6,72 +6,7 @@
 #include <QMatrix4x4>
 #include <QVector3D>
 #include <QCommandLineParser>
-
 #include "ViewWindow.h"
-
-void printQMat4x4(QMatrix4x4 mat) {
-    mat = mat.transposed();
-    for(int i=0;i<16;i++) {
-        if(i%4==0)
-            printf("\n");
-        printf("%f ",*(mat.data()+i));
-    }
-    printf("\n*******************\n");
-}
-
-void debugTransform() {
-    // intrinsic camera matrix of data camera
-    QMatrix4x4 Kci (512.0f,  0.0f,           0.0f,  512.0f,
-                    0.0f,           512.0f,    0.0f,  512.0f,
-                    0.0f,           0.0f,           1.0f,  0.0f,
-                    0.0f,           0.0f,           0.0f,  1.0f);
-
-    // Projection Matrix for data camera
-    QMatrix4x4 Pc (1.0f,     0.0f,      0.0f,  0.0f,
-                    0.0f,      1.0f,      0.0f,  0.0f,
-                    0.0f,      0.0f,      1.0f,  -8,
-                    0.0f,      0.0f,      1.0f,  0.0f);
-
-    // Inverted View Transformation matrix for virtual camera
-    QMatrix4x4 Vk;
-    Vk.lookAt(QVector3D(1.5, 1, -0.1),QVector3D(1.5,1 ,0.9),QVector3D(0,1,0));
-    QMatrix4x4 Vk_inv = Vk.inverted();
-
-    // Inverted Projection matrix for virtual camera
-    QMatrix4x4 Pc_inv = Pc.inverted();
-
-    // Inverted intrinsic camera matrix for virtual camera
-    QMatrix4x4 Kck_inv = Kci.inverted();
-
-    QMatrix4x4 Vc;
-    Vc.lookAt(QVector3D(0, 4, 0),QVector3D(0,4,1),QVector3D(0,1,0));
-//    Vc = Vc.transposed();
-
-    QVector4D pk(600,600,0,1);
-
-    printf("Kci=\n");
-    printQMat4x4(Kci);
-
-    printf("Pc=\n");
-    printQMat4x4(Pc);
-
-    printf("Vc=\n");
-    printQMat4x4(Vc);
-
-    printf("Vk_inv=\n");
-    printQMat4x4(Vk_inv);
-
-    printf("Pc_inv=\n");
-    printQMat4x4(Pc_inv);
-
-    printf("Kck_inv=\n");
-    printQMat4x4(Kck_inv);
-
-    QVector4D pi = (Kci * Pc * Vc * Vk_inv * Pc_inv * Kck_inv) * pk;
-    pi = pi/pi[3];
-
-    printf("pi = %f %f %f %f\n", pi[0], pi[1], pi[2], pi[3]);
-}
 
 int main(int argc, char *argv[])
 {
@@ -92,11 +27,11 @@ int main(int argc, char *argv[])
     QSurfaceFormat::setDefaultFormat(fmt);
 
     QString data_dir = "../Data/checkboard_plane";
-    QString output_dir = "../Output/checkboard_plane";
+    QString output_dir = "../Output";
     const QStringList args = parser.positionalArguments();
-    if( args.length()>0 )
+    if( args.length() > 0 )
         data_dir = args[0];
-    if( args.length()>1 )
+    if( args.length() > 1 )
         output_dir = args[1];
     ViewerWindow window;
     window.show();
