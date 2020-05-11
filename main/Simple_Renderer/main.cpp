27c0200c0231b9dd5e4a79a0cc8424f021a31cca
Pawel Burzynski <pawel.burzynski98@gmail.com>
2020-05-11 19:56:20 +0100

    cleaning

--- main/Simple_Renderer/main.cpp
+++ main/Simple_Renderer/main.cpp
@@ -12,33 +12,43 @@
 
 int main(int argc, char *argv[])
 {
-//  debugTransform();
     QApplication app(argc, argv);
 
     QCommandLineParser parser;
-    parser.setApplicationDescription("Simple Gausian Interpolation Renderer");
+    parser.setApplicationDescription("Image-Based Renderer");
     parser.addHelpOption();
     //parser.addVersionOption();
     parser.addPositionalArgument("training_dir", QString("Directory with training images."));
     parser.addPositionalArgument("output_dir", QString("Directory with output images."));
+    parser.addPositionalArgument("rendering_alg", QString("Rendering algorithm to be chosen from: LightFieldInterpolation,ViewDependentTextureMapping,DeferredNeuralRenderer"));
     parser.process(app);
-
 
     QSurfaceFormat fmt;
     fmt.setSamples(4);
     QSurfaceFormat::setDefaultFormat(fmt);
 
-    QString data_dir = "../Data/checkboard_plane";
-    QString output_dir = "../Output";
+    QString data_dir = "";
+    QString output_dir = "";
+    Renderer *renderer;
     const QStringList args = parser.positionalArguments();
     if( args.length() > 0 )
         data_dir = args[0];
     if( args.length() > 1 )
         output_dir = args[1];
+    if( args.length() > 2 ) {
+        if(args[2] == "LightFieldInterpolation" ) {
+            renderer  = new LightFieldInterpolation(data_dir);
+        }
+        if(args[2] == "ViewDependentTextureMapping" ) {
+            renderer  = new ViewDependentTextureMapping(data_dir);
+        }
+    } else {
+        renderer = new ViewDependentTextureMapping(data_dir);
+    }
+
     //renderer to use
-    ViewDependentTextureMapping renderer(data_dir);
-    ViewerWindow window(&renderer);
+    ViewerWindow window(renderer);
     window.show();
-    //window.generateEvaluationOutput(data_dir, output_dir);
+    window.generateEvaluationOutput(data_dir, output_dir);
     return app.exec();
 }
